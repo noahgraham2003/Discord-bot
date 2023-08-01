e0cd91d27ddf86540e8a63ac4d5b4de3224c134d
Noah Graham <70675251+noahgraham2003@users.noreply.github.com>
2023-07-31 23:12:54 -0500

    bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTEwNzUwNDg1NzEyMTgyODg2Ng.G6V7p4.nwbF0VEGk1VFJH5jfRmqH_cuXNipcUVFkMArB0'
+    TOKEN = ''
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
